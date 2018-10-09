ddad9df4c6562f2a0f119b6ebaeae69c6e379d66
--global <owan.hunte@hyuna.bb>
2018-10-09 12:21:51 -0400

    Uses the os module to replace 'Hello World' with a user-targeted greeting.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log('Starting app.');
 
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFileSync('greetings.txt', 'Hello World!');
+var user = os.userInfo();
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
