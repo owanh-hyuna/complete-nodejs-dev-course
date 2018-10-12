5413e531d179c1a173c794a585da9d367e7d307f
--global <owan.hunte@hyuna.bb>
2018-10-12 14:13:31 -0400

    Adds simple error checking in the Geocode API request callback.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
 
-// Read .env vars into Node.
-require('dotenv').config();
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
 	.options({
@@ -16,17 +14,10 @@ const argv = yargs
 	.help()
 	.alias('help', 'h').argv;
 
-let address = argv.address;
-let encodedAddress = encodeURIComponent(address);
-
-request(
-	{
-		url: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_API_KEY}&address=${encodedAddress}`,
-		json: true
-	},
-	(error, response, body) => {
-		console.log(`Address: ${body.results[0].formatted_address}`);
-		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+	if (errorMessage) {
+		console.log(errorMessage);
+	} else {
+		console.log(JSON.stringify(results, undefined, 2));
 	}
-);
+});
