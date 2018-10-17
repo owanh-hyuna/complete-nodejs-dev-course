8de8977b938b68a6a77fdd7a624d61617e214e40
--global <owan.hunte@hyuna.bb>
2018-10-17 11:15:14 -0400

    Creates simple Express middleware.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,31 @@
 'use strict';
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', err => {
+		if (err) {
+			console.log('Unable to append to server.log.');
+		}
+	});
+	next();
+});
+
+//app.use((req, res, next) => {
+//	res.render('maintenance.hbs', {
+//		pageTitle: "We'll be right back"
+//	});
+//});
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
